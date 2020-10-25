d70776774893b4581a2588fcb6d2d1a81177bd81
Bobjack85 <72252775+Bobjack85@users.noreply.github.com>
2020-10-25 13:23:12 +0300

    Revert "Lesson1"

--- lesson1_2.py
+++
@@ -1,7 +0,0 @@
-time = int(input("Enter time in sec"))
-hours = time // 3600
-minutes = (time - hours * 3600) // 60
-seconds = time - (hours * 3600 + minutes * 60)
-print(f"Time: hh:mm:ss   {hours} : {minutes} : {seconds}"
-
-
